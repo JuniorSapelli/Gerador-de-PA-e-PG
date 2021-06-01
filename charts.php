8a6005ab0046ebe791e38c3049cc311e36a0becb
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2021-05-31 21:38:06 -0300

    charts

--- charts.php
+++ charts.php
@@ -4,8 +4,34 @@
     $menu = (isset($_POST['menu']) ? $_POST['menu'] : "");
 ?>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gráfico da Progressão Gerada</title>
-</head>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Quantidade', 'Progressão'  ],
+          ['1',  1000    ],
+          ['2',  1170    ],
+          ['3',  660     ],
+          ['4',  1030    ]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
