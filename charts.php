e4a8ad05faa2acfbdbef25c79fc2cd2204fd8a8e
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2021-05-31 21:45:27 -0300

    charts

--- charts.php
+++ charts.php
@@ -7,31 +7,35 @@
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+        google.charts.load('current', {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(drawBasic);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Quantidade', 'Progressão'  ],
-          ['1',  1000    ],
-          ['2',  1170    ],
-          ['3',  660     ],
-          ['4',  1030    ]
+    function drawBasic() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Progressao');
+
+        data.addRows([
+            [0, 5],   [1, 50],  [2, 500],  [3, 5000]
         ]);
 
         var options = {
-          title: 'Company Performance',
-          curveType: 'function',
-          legend: { position: 'bottom' }
+            hAxis: {
+            title: 'Quantidade'
+            },
+            vAxis: {
+            title: 'Valor'
+            }
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
         chart.draw(data, options);
-      }
+    }
     </script>
   </head>
   <body>
-    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+    <div id="chart_div" style="width: 900px; height: 500px"></div>
   </body>
 </html>
